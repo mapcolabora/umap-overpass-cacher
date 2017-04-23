fd17bd02950c8fa7f7c84dd01d509f6cb40d2b8f
Alejandro Suarez <alejandroscf@gmail.com>
2017-04-23 21:14:30 +0200

    Added caching program

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source config.sh
+
+MAX_SLEEP=${MAX_SLEEP:-600}
+DATA_FOLDER=${DATA_FOLDER:-/var/www/html/}
+
+
+while read url; do
+   out=$( echo "$url"|md5sum|cut -d " " -f 1 ).json
+   echo "Downloading quey to $DATA_FOLDER/$out"
+   wget -q -O "$DATA_FOLDER/$out" "$url"
+   RAND=$(( $RANDOM % $MAX_SLEEP ))
+   echo "Sleeping for $RAND seconds between queries"
+   sleep $RAND
+
+done<urls.txt
