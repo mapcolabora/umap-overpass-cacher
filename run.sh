5367eac664f1ec244eec855556aed2c9fe647669
Alejandro Suarez <alejandroscf@gmail.com>
2017-04-23 21:30:12 +0200

    Added parameters for debugging

--- run.sh
+++ run.sh
@@ -5,13 +5,27 @@ source config.sh
 MAX_SLEEP=${MAX_SLEEP:-600}
 DATA_FOLDER=${DATA_FOLDER:-/var/www/html/}
 
+case $1 in
+   --dry-run|-dr)
+      DR=1
+      NOW=1
+      ;;
+   --now|-n)
+      NOW=1
+      ;;
+esac
+
 
 while read url; do
    out=$( echo "$url"|md5sum|cut -d " " -f 1 ).json
    echo "Downloading quey to $DATA_FOLDER/$out"
-   wget -q -O "$DATA_FOLDER/$out" "$url"
+   if ! [[ $DR ]]; then
+       wget -q -O "$DATA_FOLDER/$out" "$url"
+   fi
    RAND=$(( $RANDOM % $MAX_SLEEP ))
    echo "Sleeping for $RAND seconds between queries"
-   sleep $RAND
+   if ! [[ $NOW ]]; then
+      sleep $RAND
+   fi
 
 done<urls.txt
